045fc3296324b945506e963b7954d3aa8bfd8f21
Brandon Pfeifer <thebpfeifer@gmail.com>
2018-07-16 00:23:31 -0500

    Added sample asset package to assist with visualizing buildings

---
+++ Assets/Scripts/InventoryDisplayController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryDisplayController : MonoBehaviour {
+
+    public GameObject InventorySystem;
+    public Text WoodCount; 
+
+	// Use this for initialization
+	void Start () {
+        int woodCount = InventorySystem.GetComponent<InventoryController>().woodCount;
+        WoodCount.text = "Wood Count: " + woodCount.ToString(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
